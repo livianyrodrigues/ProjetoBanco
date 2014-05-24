1c239bff546bc87dabcc1fbffc7bd4722f3a5e33
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-24 19:52:33 -0300

    Versão 0.1

---
+++ ProjetoBanco/Banco.java
@@ -0,0 +1,17 @@
+public class Banco{
+    private String nome;
+    private int numero;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public int getNumero(){
+        return numero;
+    }
+}
