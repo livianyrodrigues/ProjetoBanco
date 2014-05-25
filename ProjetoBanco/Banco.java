76dec005dd45a0ac99063b663772b3a1f8e89734
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-24 21:02:06 -0300

    Versão 0.3

--- ProjetoBanco/Banco.java
+++ ProjetoBanco/Banco.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class Banco{
     private String nome;
     private int numero;
+    private List<Agencia> agencias = new ArrayList<Agencia>();
+    
+    
+    public void cadastrarAgencia(Agencia agencia){
+        agencias.add(agencia);
+    }
+    
+    public Agencia PesquisarAgenciaPorNumero(int numero){
+        for (Agencia agencia : agencias){
+            if (agencia.getNumero() == numero){
+                return agencia;
+            }
+        }
+        throw new RuntimeException("Agencia não encontrada: " +numero);
+    }
     
     public void setNome(String nome){
         this.nome = nome;
